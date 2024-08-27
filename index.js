ae14ad34047697a32f0324499da03076c251625a
vikkto23 <www.sylvick011@gmail.com>
2024-08-27 17:17:01 +0100

    weather

---
+++ index.js
@@ -0,0 +1,38 @@
+// ACCESSING ALL THE HTML COMPONENTS REQUIRED TO PERFORM ACTIONS ON.
+let button = document.querySelector('.button')
+let inputvalue = document.querySelector('.inputValue')
+let nameVal = document.querySelector('.name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+
+
+function weatherSearch(){
+
+    // Fection data from open weather API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=108dd9a67c96f23039937fe6f3c91963`)
+    .then(response => response.json())
+    .then(
+        displayData)
+    .catch(err => alert('Wrong City name'));
+}
+// ADDING EVENT LISTENER TO SEARCH BUTTON  
+button.addEventListener('click', function(){
+    weatherSearch();
+
+})
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    weatherSearch();
+  }
+});
+
+// Function to diplay weather on html document
+const displayData=(weather)=>{
+    temp.innerText=`${weather.main.temp}°C`
+    desc.innerText=`${weather.weather[0].main}`
+  
+}
+
+    
